45fad2b6ffec696e6fe1c888957636183dce5290
Hans Oskar Aaviksoo <ha@restake.net>
2024-04-15 14:40:53 +0300

    RI-1252: Add support for building Solana binaries

---
+++ scripts/solana/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd solana
+mkdir bin
+
+export CI_COMMIT=$(git rev-parse HEAD)
+export CARGO_BUILD_TARGET="x86_64-unknown-linux-gnu"
+export CARGO_BUILD_RUSTFLAGS="-C target-cpu=${DEPOT_CPU}"
+export CARGO_INCREMENTAL="0"
+
+cargo build --profile release --bin solana-validator
+
+build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
+
+echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
+    mv -v "target/${CARGO_BUILD_TARGET}/release/${binary}" "${path}"
+done
