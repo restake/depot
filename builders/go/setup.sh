40c8b0f025eebba8ab0b180f1f724b002ad0ec49
Hans Oskar Aaviksoo <ha@restake.net>
2024-04-25 13:08:18 +0300

    Prefer manually installed tools to pre-existing

--- builders/go/setup.sh
+++ builders/go/setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 install_dir="${GITHUB_WORKSPACE}/tools/go-${DEPOT_BUILDER_VERSION}"
 
 if ! [ -d "${install_dir}" ]; then
+    echo ">>> installing Go with version ${DEPOT_BUILDER_VERSION}"
     mkdir -p "${install_dir}"
-    curl -o go.tar.gz "https://dl.google.com/go/go${DEPOT_BUILDER_VERSION}.linux-amd64.tar.gz"
-    tar --strip-components=1 -C "${install_dir}" -xzf go.tar.gz
+    curl -L -O "https://go.dev/dl/go${DEPOT_BUILDER_VERSION}.linux-amd64.tar.gz"
+    tar --strip-components=1 -C "${install_dir}" -xf "go${DEPOT_BUILDER_VERSION}.linux-amd64.tar.gz"
 fi
 
-export PATH="${PATH}:${install_dir}/bin"
+export PATH="${install_dir}/bin:${PATH}"
 
 echo "PATH=${PATH}" >> "${GITHUB_ENV}"
 echo ">>> $(go version)"
