4a671ce5077bc740e1f9c5e1e206dc35dc666917
Hans Oskar Aaviksoo <ha@restake.net>
2024-05-13 14:37:16 +0300

    Add support for Babylon binaries

---
+++ scripts/babylon/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd babylon
+
+# Grab static libwasmvm
+wasmvm_version="$(go list -m all | grep -F "github.com/CosmWasm/wasmvm" | awk '{print $2}')"
+curl -JLO "https://github.com/CosmWasm/wasmvm/releases/download/${wasmvm_version}/libwasmvm_muslc.x86_64.a"
+ln -s libwasmvm_muslc.x86_64.a libwasmvm.x86_64.a
+
+make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true build
+
+build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
+
+echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
+    mv -v "${GITHUB_WORKSPACE}/babylon/bin/${binary}" "${path}"
+done
