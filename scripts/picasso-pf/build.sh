303e22a5d366ec3f3f521b392ad84d5486f3c503
Hans Oskar Aaviksoo <ha@restake.net>
2024-05-15 14:05:55 +0300

    RI-1365: Build Picasso price feed binaries

---
+++ scripts/picasso-pf/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd picasso-pf
+mkdir bin
+
+cargo build --release --package price-feed
+
+build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
+
+echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
+    mv -v "target/release/${binary}" "${path}"
+done
