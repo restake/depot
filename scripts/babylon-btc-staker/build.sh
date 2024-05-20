2ce3eb98af0ae1f3ac71df69785d8df76c707812
Hans Oskar Aaviksoo <ha@restake.net>
2024-05-20 16:24:22 +0300

    Add support for building Babylon BTC staker

---
+++ scripts/babylon-btc-staker/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "${DEPOT_PROJECT_NAME}"
+mkdir bin
+
+# Grab static libwasmvm
+wasmvm_version="$(go list -m all | grep -F "github.com/CosmWasm/wasmvm" | awk '{print $2}')"
+curl -JLO "https://github.com/CosmWasm/wasmvm/releases/download/${wasmvm_version}/libwasmvm_muslc.x86_64.a"
+ln -s libwasmvm_muslc.x86_64.a libwasmvm.x86_64.a
+
+make LINK_STATICALLY=true build
+
+build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
+
+echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
+    mv -v "${GITHUB_WORKSPACE}/babylon-fp/build/${binary}" "${path}"
+done
