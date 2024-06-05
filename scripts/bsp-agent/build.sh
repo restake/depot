41633d4e97fc959a638c7bf6964372fefa73c73b
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2024-06-05 15:47:33 +0300

    RI-1444: Publish block-ethereum.avsc with bsp-agent binary

--- scripts/bsp-agent/build.sh
+++ scripts/bsp-agent/build.sh
@@ -11,3 +11,6 @@ build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
     mv -v "${GITHUB_WORKSPACE}/bsp-agent/bin/${binary}" "${path}"
 done
+
+# Additional files required during runtime
+cp -v codec/block-ethereum.avsc bin/
