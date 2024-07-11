3307c19bcdfc057721b05fe3dcfd318549041457
Markkus Millend <mm@liangroup.io>
2024-07-11 14:00:19 +0200

    Rename Composable Centauri to Picasso

--- scripts/centauri/build.sh
+++ scripts/centauri/build.sh
@@ -17,5 +17,5 @@ ldd ./bin/picad || :
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
-    mv -v "${GITHUB_WORKSPACE}/centauri/bin/${binary}" "${path}"
+    mv -v "${GITHUB_WORKSPACE}/picasso/bin/${binary}" "${path}"
 done
