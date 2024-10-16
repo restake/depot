a4182ddff8d76ef57b2017a5754064a67a2246cd
Hans Oskar Aaviksoo <ha@restake.net>
2024-10-16 11:27:36 +0300

    RES-1545: Update pathfinder directory

--- scripts/pathfinder/build.sh
+++ scripts/pathfinder/build.sh
@@ -14,5 +14,5 @@ cargo build --release --bin pathfinder
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
-    mv -v "${GITHUB_WORKSPACE}/tmkms/target/release/${binary}" "${path}"
+    mv -v "${GITHUB_WORKSPACE}/pathfinder/target/release/${binary}" "${path}"
 done
