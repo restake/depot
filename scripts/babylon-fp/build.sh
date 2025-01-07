789d008f1f29ec4f87c4a3e9b1c35279a6318878
Karl Markus Jõgila <karlmjogila@gmail.com>
2025-01-08 00:02:29 +0200

    RES-2030: Update go version for babylon fp

--- scripts/babylon-fp/build.sh
+++ scripts/babylon-fp/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
 cd "${DEPOT_PROJECT_NAME}"
 mkdir bin
 
 # Grab static libwasmvm
-wasmvm_version="$(go list -json -m all | jq -cr 'select(.Path == "github.com/CosmWasm/wasmvm") | .Replace.Version // .Version')"
+wasmvm_version="$(go list -json -m all | jq -cr 'select(.Path == "github.com/CosmWasm/wasmvm/v2") | .Replace.Version // .Version')"
 curl -JLO "https://github.com/CosmWasm/wasmvm/releases/download/${wasmvm_version}/libwasmvm_muslc.x86_64.a"
 ln -s libwasmvm_muslc.x86_64.a libwasmvm.x86_64.a
 
