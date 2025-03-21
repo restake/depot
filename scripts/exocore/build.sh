6cb21752f5e12049c56768d183ccaa429bb6f985
Karl Markus Jõgila <karlmjogila@gmail.com>
2025-03-21 13:20:00 +0200

    RES-2338: Add depot build script for exocore(now imua)

--- scripts/exocore/build.sh
+++ scripts/exocore/build.sh
@@ -9,5 +9,5 @@ make build
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
-    mv -v "${GITHUB_WORKSPACE}/exocore/build/bin/${binary}" "${path}"
+    mv -v "${GITHUB_WORKSPACE}/exocore/build/${binary}" "${path}"
 done
