6b3686ee3c663291031379f75d0195e2af358441
Hans Oskar Aaviksoo <ha@restake.net>
2025-04-04 13:38:20 +0300

    fix: Update packages for rust tooling

--- builders/rust/deps.sh
+++ builders/rust/deps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+sudo apt update
+
 sudo apt-get install -y --no-install-recommends build-essential libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler
 
 echo "cached-tooling-path=${GITHUB_WORKSPACE}/tools/rust/cargo" >> "${GITHUB_OUTPUT}"
