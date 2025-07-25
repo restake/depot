10c713951af065ea51a5333e8aea3a1f84a23aa0
Hans Oskar Aaviksoo <ha@restake.net>
2025-07-25 13:36:20 +0300

    RES-3215: Link static

--- scripts/cronos/build.sh
+++ scripts/cronos/build.sh
@@ -6,6 +6,19 @@ mkdir bin
 
 make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true build
 
+export CGO_ENABLED=1
+export CGO_LDFLAGS="-static -L."
+export GOFLAGS=""
+
+make CC="x86_64-linux-musl-gcc" LEDGER_ENABLED=false LINK_STATICALLY=true build
+
+# Verify the binary is static
+echo "Checking if binary is static:"
+if ldd bin/persistenceCore; then
+    echo "Not a static binary"
+    exit 1
+fi
+
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
