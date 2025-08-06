70bc63a411832c741b85aa5f325e5fef555f3416
Hans Oskar Aaviksoo <ha@restake.net>
2025-08-06 17:41:15 +0300

    Update Go version for omniflix

--- scripts/omniflixhub/build.sh
+++ scripts/omniflixhub/build.sh
@@ -4,7 +4,19 @@ set -euo pipefail
 cd "${DEPOT_PROJECT_NAME}"
 mkdir bin
 
-make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true build
+# Grab static libwasmvm
+wasmvm_version="$(go list -json -m all | jq -cr 'select(.Path == "github.com/CosmWasm/wasmvm/v2") | .Replace.Version // .Version')"
+curl -JLO "https://github.com/CosmWasm/wasmvm/releases/download/${wasmvm_version}/libwasmvm_muslc.x86_64.a"
+ln -s libwasmvm_muslc.x86_64.a libwasmvm.x86_64.a
+
+make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." GOLDFLAGS="-linkmode=external -extldflags '-Wl,-z,muldefs -static'" LEDGER_ENABLED=false build
+
+# Verify the binary is static
+echo "Checking if binary is static:"
+if ldd bin/persistenceCore; then
+    echo "Not a static binary"
+    exit 1
+fi
 
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
