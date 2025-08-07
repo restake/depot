764cb42fa79d703eaa9e8809449cc405fbd2f7b0
Hans Oskar Aaviksoo <ha@restake.net>
2025-08-07 15:44:23 +0300

    RES-3260: Remove static wasmvm

--- scripts/omniflixhub/build.sh
+++ scripts/omniflixhub/build.sh
@@ -4,12 +4,7 @@ set -euo pipefail
 cd "${DEPOT_PROJECT_NAME}"
 mkdir bin
 
-# Grab static libwasmvm
-wasmvm_version="$(go list -json -m all | jq -cr 'select(.Path == "github.com/CosmWasm/wasmvm/v2") | .Replace.Version // .Version')"
-curl -JLO "https://github.com/CosmWasm/wasmvm/releases/download/${wasmvm_version}/libwasmvm_muslc.x86_64.a"
-ln -s libwasmvm_muslc.x86_64.a libwasmvm.x86_64.a
-
-make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." GOLDFLAGS="-linkmode=external -extldflags '-Wl,-z,muldefs -static'" LEDGER_ENABLED=false build
+make build
 
 # Verify the binary is static
 echo "Checking if binary is static:"
