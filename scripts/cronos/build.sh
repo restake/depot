0a553efcbf2f7cf18d6541c3e79a818f9d7bf658
Hans Oskar Aaviksoo <ha@restake.net>
2025-08-07 15:31:48 +0300

    Fix: Remove unnecessary lines from Cronos build script

--- scripts/cronos/build.sh
+++ scripts/cronos/build.sh
@@ -4,17 +4,11 @@ set -euo pipefail
 cd "${DEPOT_PROJECT_NAME}"
 mkdir bin
 
-make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true build
-
-export CGO_ENABLED=1
-export CGO_LDFLAGS="-static -L."
-export GOFLAGS=""
-
-make CC="x86_64-linux-musl-gcc" LEDGER_ENABLED=false LINK_STATICALLY=true build
+make CC="x86_64-linux-musl-gcc" CGO_ENABLED=1 CGO_LDFLAGS="-static -L." LEDGER_ENABLED=false LINK_STATICALLY=true build
 
 # Verify the binary is static
 echo "Checking if binary is static:"
-if ldd bin/persistenceCore; then
+if ldd bin/chain-maind; then
     echo "Not a static binary"
     exit 1
 fi
