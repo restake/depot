d3100adeb00114e5b6814bc21a8b6fbbbfaf0997
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2025-08-11 12:32:12 +0300

    try 6

--- scripts/bandchain/build.sh
+++ scripts/bandchain/build.sh
@@ -4,17 +4,16 @@ set -euo pipefail
 cd "${DEPOT_PROJECT_NAME}"
 mkdir bin
 
-# Grab static libwasmvm
-wasmvm_version="$(go list -json -m all | jq -cr 'select(.Path == "github.com/CosmWasm/wasmvm/v2") | .Replace.Version // .Version')"
-curl -JLO "https://github.com/CosmWasm/wasmvm/releases/download/${wasmvm_version}/libwasmvm_muslc.x86_64.a"
-ln -s libwasmvm_muslc.x86_64.a libwasmvm.x86_64.a
+go_owasm_version="$(go list -json -m all | jq -cr 'select(.Path == "github.com/bandprotocol/go-owasm") | .Replace.Version // .Version')"
+curl -JLO "https://github.com/bandprotocol/go-owasm/releases/download/${go_owasm_version}/libgo_owasm_muslc.x86_64.a"
+ln -s libgo_owasm_muslc.x86_64.a libgo_owasm.x86_64.a
 
-# BandChain uses 'make install' which installs to Go bin directory
-make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true install
+mkdir -p build
+make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true GOBIN="build" install
 
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
     # 'make install' puts the binary in the Go bin directory
-    mv -v "${GITHUB_WORKSPACE}/tools/go-1.24.3/bin/${binary}" "${path}"
+    mv -v "build/${binary}" "${path}"
 done
