50e221d7a3ecdfd6893f8ad56eeffcc93ee53c8f
Hans Oskar Aaviksoo <ha@restake.net>
2025-08-11 11:22:37 +0300

    RES-3284: Make build missing for Band use make install

--- scripts/bandchain/build.sh
+++ scripts/bandchain/build.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 cd "${DEPOT_PROJECT_NAME}"
 mkdir bin
 
-make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true build
+# BandChain uses 'make install' which installs to Go bin directory
+make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true install
 
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
-    mv -v "${GITHUB_WORKSPACE}/bandchain/build/${binary}" "${path}"
+    # 'make install' puts the binary in the Go bin directory
+    mv -v "${GITHUB_WORKSPACE}/tools/go-1.24.3/bin/${binary}" "${path}"
 done
