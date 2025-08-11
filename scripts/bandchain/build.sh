d19d736d55dbeccd217a28390e7cb785759114e3
Hans Oskar Aaviksoo <ha@restake.net>
2025-08-11 12:39:12 +0300

    try 7

--- scripts/bandchain/build.sh
+++ scripts/bandchain/build.sh
@@ -9,11 +9,10 @@ curl -JLO "https://github.com/bandprotocol/go-owasm/releases/download/${go_owasm
 ln -s libgo_owasm_muslc.x86_64.a libgo_owasm.x86_64.a
 
 mkdir -p build
-make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true GOBIN="build" install
+make CC="x86_64-linux-musl-gcc" CGO_LDFLAGS="-L." LEDGER_ENABLED=false LINK_STATICALLY=true GOBIN="${GITHUB_WORKSPACE}/${DEPOT_PROJECT_NAME}/build" install
 
 build_binaries="$(deno run --allow-read --allow-env ../utils/binaries.ts)"
 
 echo "${build_binaries}" | jq -r 'to_entries[] | "\(.key) \(.value)"' | while read -r binary path; do
-    # 'make install' puts the binary in the Go bin directory
     mv -v "build/${binary}" "${path}"
 done
